45706d5c5ecc1e717f4047d4b49fdb6da7831a0e
WiuAvila2 <wiuneivetom.avila@outlook.com>
2025-06-24 20:10:30 -0300

    criando componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-
+//css
 import './App.css';
 
+//react
+import { useCallback, useEffect, useState } from 'react';
+
+//data
+import { wordsList } from './data/words';
+
+//components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const satges = [
+  {id: 1 , name: "start"},
+  {id: 2 , name: "game"},
+  {id: 3 , name: "end"},
+];
+
 function App() {
+  const [gameStage, setGameStage] = useState(satges[0].name);
+  const [words] = useState(wordsList);
+
+  console.log(words);
+
   return (
-    <h2>Secret Word</h2>
+    <div className='App'>
+      {gameStage === 'start' && <StartScreen/>}
+      {gameStage === 'game' && <Game/>}
+      {gameStage === 'game' && <GameOver/>}
+    </div>
   );
 }
 
