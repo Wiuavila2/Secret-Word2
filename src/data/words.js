ff3684b4b532c430205fc04c52e4b44d8b0d0928
WiuAvila2 <wiuneivetom.avila@outlook.com>
2025-06-23 22:55:58 -0300

    ajuste inicial

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ["fusca", "corsa", "gol", "palio", "celta"],
+  fruta: ["maçã", "banana", "uva", "laranja", "melancia"],
+  corpo: ["braço", "perna", "cabeça", "joelho", "mão"],
+  computador: ["mouse", "teclado", "monitor", "gabinete", "processador"],
+  programação: ["javascript", "react", "variável", "função", "objeto"],
+  alimento: ["arroz", "feijão", "carne", "pão", "leite"]
+};
